5b7b8f540eea4c6c50ac2616519778732641764e
Tim Hjermitslev Keren Gjøderum <6584419+lajtmaN@users.noreply.github.com>
2024-08-02 21:01:36 +0200

    feat: if weekend check food for next week instead

--- app/services/date.ts
+++ app/services/date.ts
@@ -3,10 +3,18 @@ export const getNextMealDate = () => {
   if (isPastLunch(baseTime)) {
     baseTime.setUTCHours(baseTime.getHours() + 24);
   }
+  if (isWeekend(baseTime)) {
+    const daysToAdd = (8 - baseTime.getDay()) % 7 || 7; // Calculate days to add to reach next Monday
+    baseTime.setDate(baseTime.getDate() + daysToAdd);
+  }
   return baseTime;
 };
 
 const isPastLunch = (time: Date) => {
   const hour = Number(time.toLocaleTimeString("da-DK", { hour: "numeric" }));
   return hour >= 13;
+};
+
+const isWeekend = (time: Date) => {
+  return time.getDay() === 0 || time.getDay() === 6;
 };
--- app/services/meal.ts
+++ app/services/meal.ts
@@ -2,16 +2,12 @@ import { getNextMealDate } from "./date";
 import { getNameForMeal } from "./meal-name";
 
 export const getCurrentMeals = async (): Promise<Meal[]> => {
-  const response = await fetch(
-    "https://www.shop.foodandco.dk/api/WeeklyMenu?restaurantId=1042&languageCode=da-DK"
-  );
-
-  const dailyMenu = getDailyMenu(await response.json());
-  if (!dailyMenu) {
+  const todaysMenu = await getTodaysMenu();
+  if (!todaysMenu) {
     return [{ text: "¯\\_(ツ)_/¯", vegeratian: false }];
   }
 
-  return dailyMenu.map<Meal>((menu) => {
+  return todaysMenu.map<Meal>((menu) => {
     return {
       originalMealName: menu.menu,
       text: getNameForMeal(menu.menu),
@@ -20,14 +16,32 @@ export const getCurrentMeals = async (): Promise<Meal[]> => {
   });
 };
 
-const getDailyMenu = (menu: Menu): DailyMenu[] | undefined => {
+const getTodaysMenu = async (): Promise<DailyMenu[] | undefined> => {
   const mealTime = getNextMealDate();
+  const mealTimeDateString = mealTime.toISOString().split("T")[0];
+  const apiRequestParams = new URLSearchParams({
+    restaurantId: "1042",
+    languageCode: "da-DK",
+    date: mealTimeDateString,
+  });
+  const response = await fetch(
+    `https://www.shop.foodandco.dk/api/WeeklyMenu?${apiRequestParams}`
+  );
+  const menu = (await response.json()) as Menu;
 
   const dayOfWeek = mealTime
     .toLocaleDateString("da-DK", { weekday: "long" })
     .toLowerCase();
-  return menu?.days.find((day) => day.dayOfWeek.toLowerCase() === dayOfWeek)
-    ?.menus;
+
+  const menuForToday = menu?.days.find(
+    (day) => day.dayOfWeek.toLowerCase() === dayOfWeek
+  )?.menus;
+
+  if (menuForToday?.[0]?.menu?.includes("Menuen er ikke klar endnu")) {
+    return undefined;
+  }
+
+  return menuForToday;
 };
 
 const VEGETARIAN_MENU = "Dagens varme vegatar";
