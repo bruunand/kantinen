377bc4c20961d9e1156121ba973e5900a0287df0
Anders Jakobsen <aja@trifork.com>
2024-09-19 13:56:42 +0200

    remove wip prefix

--- app/services/image.ts
+++ app/services/image.ts
@@ -39,7 +39,7 @@ export const getImageBackground = async (
 export const getCacheKey = (date: Date, theme: Theme): string => {
   // Convert Date object into ISO Date string (no timestamp) (example: 2024-07-31)
   const dateString = date.toISOString().split("T")[0];
-  return `wip-${theme}-${dateString}`;
+  return `${theme}-${dateString}`;
 };
 
 const FALLBACK_IMAGE = "https://picsum.photos/id/292/1900/1300";
