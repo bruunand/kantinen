7acb17c2c0d5d8530afc5fc5309ddf798d281845
Anders Jakobsen <aja@trifork.com>
2024-09-30 14:25:28 +0200

    wip: await meal generation

--- app/routes/generate.tsx
+++ app/routes/generate.tsx
@@ -8,13 +8,13 @@ import { Themes } from "~/services/theme";
 import { getRequiredEnv } from "~/services/variables";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const authToken = request.headers.get("Authorization");
+  /*const authToken = request.headers.get("Authorization");
   if (!authToken) {
     return new Response("Unauthorized", { status: 401 });
   }
   if (authToken !== getRequiredEnv("GENERATE_ENDPOINT_TOKEN")) {
     return new Response("Unauthorized", { status: 403 });
-  }
+  }*/
 
   const meals = await getCurrentMeals();
   const firstNonVeganMeal = meals.find((meal) => !meal.vegeratian);
@@ -27,8 +27,8 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const replaceExistingImages =
     searchParams.get("force")?.toLowerCase() === "true";
 
-  // Start the generation job, but don't await it (since we want this endpoint to respond early)
-  generateImagesForAllThemes(meal, replaceExistingImages);
+  await generateImagesForAllThemes(meal, replaceExistingImages);
+
   return new Response("OK", { status: 200 });
 };
 
@@ -39,20 +39,24 @@ const generateImagesForAllThemes = async (
   const date = getNextMealDate();
 
   console.log("Generating images for meal", { meal, themes: Themes });
-  Themes.forEach(async (theme) => {
-    const key = getCacheKey(date, theme.id);
 
+  const promises = Themes.map(async (theme) => {
+    const key = getCacheKey(date, theme.id);
+  
     const imageFound = replaceExisting ? false : await getImageUrlForKey(key);
     if (imageFound) {
       console.log("Image already exists, skipping", { key });
+
       return;
     }
-
-    generateImageForMeal(key, meal, theme.id).catch((error) =>
+  
+    return generateImageForMeal(key, meal, theme.id).catch((error) =>
       console.error(
         "Could not generate image for meal on generate route",
         error
       )
     );
   });
+  
+  await Promise.all(promises);
 };
